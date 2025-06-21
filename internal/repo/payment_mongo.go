1acc36cff8f1afdfde04f1f8c47f48567eb21cf9
sam <84742666+findsam@users.noreply.github.com>
2025-06-21 11:07:22 +0100

    feat: payment repo

---
+++ internal/repo/payment_mongo.go
@@ -0,0 +1,19 @@
+package repo
+
+import (
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+const PAYMENT_DB_NAME = "payment"
+
+type PaymentRepository interface {}
+
+type PaymentRepositoryImpl struct {
+	db *mongo.Database
+}
+
+func NewPaymentRepositoryImpl(db *mongo.Database) *PaymentRepositoryImpl {
+	return &PaymentRepositoryImpl{
+		db: db,
+	}
+}
--- internal/service/payment_service.go
+++ internal/service/payment_service.go
@@ -1,8 +1,11 @@
 package service
 
+import "github.com/findsam/auth-micro/internal/repo"
+
 type PaymentService struct {
+	repo repo.PaymentRepository
 }
 
-func NewPaymentService() *PaymentService {
-	return &PaymentService{}
+func NewPaymentService(repo repo.PaymentRepository) *PaymentService {
+ return &PaymentService{repo: repo}
 }
