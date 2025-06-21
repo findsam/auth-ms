eb9e3ca92cd203c5eb6c3ab138b69c93cb7e28dd
sam <84742666+findsam@users.noreply.github.com>
2025-06-21 11:20:28 +0100

    feat: idiomatic abstractions

--- cmd/root.go
+++ cmd/root.go
@@ -7,36 +7,45 @@ import (
 	"github.com/findsam/auth-micro/internal/repo"
 	"github.com/findsam/auth-micro/internal/router"
 	"github.com/findsam/auth-micro/internal/service"
-	"github.com/findsam/auth-micro/pkg/mongo"
+	m "github.com/findsam/auth-micro/pkg/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
-func Execute() {
-	db, err := mongo.New()
+func Execute() error {
+	db, err := m.New()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	deps := &router.Handlers{
+		Store: buildStoreHandler(db),
+		Payment: buildPaymentHandler(db),
+		User: buildUserHandler(db),
+	}
+
+	router := router.New("8080", deps)
+	return router.Start()
+
+}
+
+
+func buildStoreHandler(db *mongo.Database) *handler.StoreHandler {
 	storeRepo := repo.NewStoreRepositoryImpl(db)
 	storeService := service.NewStoreService(storeRepo)
 	storeHandler := handler.NewStoreHandler(storeService)
+	return storeHandler
+}
 
+func buildUserHandler(db *mongo.Database) *handler.UserHandler {
 	userRepo := repo.NewUserRepositoryImpl(db)
 	userService := service.NewUserService(userRepo)
 	userHandler := handler.NewUserHandler(userService)
+	return userHandler
+}
 
-
+func buildPaymentHandler(db *mongo.Database) *handler.PaymentHandler {
 	paymentRepo := repo.NewPaymentRepositoryImpl(db)
 	paymentService := service.NewPaymentService(paymentRepo)
 	paymentHandler := handler.NewPaymentHandler(paymentService)
-
-	router := router.New("8080", &router.Handlers{
-		User:  userHandler,
-		Store: storeHandler,
-		Payment: paymentHandler,
-	})
-
-	err = router.Start()
-	if err != nil {
-		log.Fatal(err)
-	}
+	return paymentHandler
 }
